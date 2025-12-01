04d38b6dfd5279b39d50d4493176edae98c6b756
Barth <derouaultbarthelemy@protonmail.com>
2025-12-01 09:28:39 +0100

    Création de la classe node

---
+++ src/Node.java
@@ -0,0 +1,16 @@
+public class Node {
+    private String label;
+
+    public Node(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String toString() {
+        return label;
+    } 
+}
+
